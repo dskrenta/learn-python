5a01c5f7ebbe8ea880de1a40a180eee59d113525
dskrenta <dskrenta@gmail.com>
2017-01-24 18:45:06 -0800

    lab1

---
+++ lab1/DavidSkrentaLab1.py
@@ -0,0 +1,24 @@
+'''
+CIS 117 Python Programing: Lab 1
+David Skrenta
+'''
+
+print("Hello, World!")
+print("Skrenta")
+print("G01032575")
+print("Students should only discuss with respect to projects, not use the same code.")
+print("Last day to drop without a \"W\" is Sunday, February 5.")
+print("The second topic of study will be Data and Operations.")
+print("Source code lines should be no longer than 79 characters.")
+print("Camel case should be used.")
+
+'''
+Hello, World!
+Skrenta
+G01032575
+Students should only discuss with respect to projects, not use the same code.
+Last day to drop without a "W" is Sunday, February 5.
+The second topic of study will be Data and Operations.
+Source code lines should be no longer than 79 characters.
+Camel case should be used.
+'''
