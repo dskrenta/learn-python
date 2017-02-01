c60e49da2a6581725ae64f27bf2bc469618e1c04
dskrenta <dskrenta@gmail.com>
2017-01-31 20:16:41 -0800

    lab2 completion

--- lab2/DavidSkrentaLab2.py
+++ lab2/DavidSkrentaLab2.py
@@ -3,42 +3,27 @@ CIS 117 Python Programing: Lab 2
 David Skrenta
 '''
 
+gid = input("Enter GID: ")
+name = input("Enter family name: ")
 
-'''
-expression1:             myId / 2
+print("My family name is " + name)
+print("My Student ID is " + gid)
 
-expression 2:            myId % 2
+myId = sum(list(map(int, str(gid))))
+nLet = len(name)
 
-expression 3:            2 + 3 + … + nLet
-
-expression 4:            myId + nLet
-
-expression 5:            abs(nLet – myId)
-
-expression 6:            (myId) / (nLet + 1100)
-
-expression 7:            (nLet % nLet) and (myId * myId)
-
-expression 8:            1 or (myId / 0)
-
-expression 9:            round(3.14, 1)
-'''
-
-myId = sum([0, 1, 0, 3, 2, 5, 7, 5])
-nLet = len("skrenta")
+print("The value of myId is " + str(myId))
+print("The number of characters in my last name is " + str(nLet))
 
 expression1 = myId / 2
 expression2 = myId % 2
 expression3 = sum(list(range(2, nLet)))
 expression4 = myId = nLet
 expression5 = abs(nLet - myId)
-expression6 = (myId) / (nLet + 1100)
+expression6 = round((myId) / (nLet + 1100), 2)
 expression7 = (nLet % nLet) and (myId * myId)
 expression8 = 1 or (myId / 0)
 expression9 = round(3.14, 1)
-
-print(myId)
-print(nLet)
 
 print(expression1)
 print(expression2)
