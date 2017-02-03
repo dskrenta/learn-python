0e13e0a98442448c74ee5f7250983f0ff3d56f13
dskrenta <dskrenta@gmail.com>
2017-02-02 23:52:06 -0800

    lab3 completion

--- lab3/DavidSkrentaLab3.py
+++ lab3/DavidSkrentaLab3.py
@@ -7,7 +7,14 @@ months = ["January", "February", "March", "April", "May", "June",
 "July", "August", "September", "November", "December"]
 
 dateString = input("Enter a date (mm/dd/yyyy): ")
+
 date = dateString.split('/')
 monthIndex = int(date[0].strip('0')) - 1
 
-print("The converted date is: " + months[monthIndex] + " " + date[1] + ", " + date[2])
+print("The converted date is: " + months[monthIndex] + " "
++ date[1].strip('0') + ", " + date[2])
+
+'''
+Enter a date (mm/dd/yyyy): 02/07/1998
+The converted date is: February 7, 1998
+'''
