f3b81165884e97d793305ce3ee46d1490dbc1db1
dskrenta <dskrenta@gmail.com>
2017-03-07 21:46:00 -0800

    finished lab6

--- lab6/DavidSkrentaLab6.py
+++ lab6/DavidSkrentaLab6.py
@@ -3,27 +3,39 @@ CIS 117 Python Programing: Lab 6
 David Skrenta
 '''
 
-def get_data(filename):
-    data_file = open(filename, encoding='utf-8')
-    data = data_file.read().split('\n')[:-1]
-    return data
-
 def is_valid(line):
     return line.isalpha() or line.isdigit() or '_' in line
 
 def main():
     results = {}
-    data = get_data('t6.py')
-    for item in data:
-        if is_valid(item):
-            if item in results:
-                results[str(item)] = [data.index(item)]
-            else:
-                results[str(item)].append(data.index(item))
-    print(results)
-
+    lines = 0
+    with open('t6.py', encoding='utf-8') as data:
+        for item in data:
+            lines += 1
+            item = item.rstrip()
+            if is_valid(item):
+                if item in results:
+                    results[item].append(lines)
+                else:
+                    results[item] = [lines]
+    for key in results.keys():
+        print(key + ": " + str(results[key]))
 
 main()
 
 '''
+stick: [20]
+art: [5]
+goodbye: [25]
+ball: [4, 19]
+orange: [15]
+carrot: [13]
+rat: [9]
+4: [10]
+dog: [6]
+_: [22]
+apple: [1, 11]
+ant: [16, 17, 18]
+pen: [8, 21]
+1: [2, 3]
 '''
