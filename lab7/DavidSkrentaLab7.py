434e888588596b0efa303d19cb8509506b560738
dskrenta <dskrenta@gmail.com>
2017-03-14 22:49:44 -0700

    lab7 complete

--- lab7/DavidSkrentaLab7.py
+++ lab7/DavidSkrentaLab7.py
@@ -3,39 +3,35 @@ CIS 117 Python Programing: Lab 7
 David Skrenta
 '''
 
-def is_valid(line):
-    return line.isalpha() or line.isdigit() or '_' in line
-
 def main():
-    results = {}
-    lines = 0
-    with open('t6.py', encoding='utf-8') as data:
-        for item in data:
-            lines += 1
-            item = item.rstrip()
-            if is_valid(item):
-                if item in results:
-                    results[item].append(lines)
+    runs = 0
+    while runs < 5:
+        results = []
+        filename = input("Please enter the file name: ")
+        with open(filename, encoding='utf-8') as data:
+            for item in data:
+                item = item.rstrip()
+                results.append(item)
+            if all(item.isdigit() for item in results):
+                if len(results) == int(results[0]):
+                    print("The sum is " + str(sum(int(num) for num in results)))
                 else:
-                    results[item] = [lines]
-    for key in results.keys():
-        print(key + ": " + str(results[key]))
+                    print("Error: file contents invalid.")
+            else:
+                print("Error: file contents invalid.")
+        runs += 1
 
 main()
 
 '''
-stick: [20]
-art: [5]
-goodbye: [25]
-ball: [4, 19]
-orange: [15]
-carrot: [13]
-rat: [9]
-4: [10]
-dog: [6]
-_: [22]
-apple: [1, 11]
-ant: [16, 17, 18]
-pen: [8, 21]
-1: [2, 3]
+Please enter the file name: bad1.dat
+Error: file contents invalid.
+Please enter the file name: bad2.dat
+Error: file contents invalid.
+Please enter the file name: bad3.dat
+Error: file contents invalid.
+Please enter the file name: bad4.dat
+Error: file contents invalid.
+Please enter the file name: good.dat
+The sum is 55
 '''
