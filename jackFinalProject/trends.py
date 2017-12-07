d2390be1384f8f784449f54f80aa7f49375513b8
dskrenta <dskrenta@gmail.com>
2017-12-06 19:54:58 -0700

    most popular tag

--- jackFinalProject/trends.py
+++ jackFinalProject/trends.py
@@ -84,17 +84,72 @@ def map_sentiment_to_state(tweets, sentiments, states, filter_terms):
 
     return sentiment_color_map
 
+def closest_state(tweet, states):
+    min_distance = 10000.0
+    closest_state = ''
+
+    for state in states:
+        distance = tweet.position().distance(state.centroid())
+
+        if distance < min_distance:
+            min_distance = distance
+            closest_state = state.abbrev()
+
+    return closest_state
+
+def most_popular_hashtag_by_state(tweets, states):
+    tweets_by_state = {}
+    tags_by_state_with_freq = {}
+    most_pop_tag_by_state = {}
+
+    for state in states:
+        tags_by_state_with_freq[state.abbrev()] = {}
+        most_pop_tag_by_state[state.abbrev()] = ''
+
+    for tweet in tweets:
+        state = closest_state(tweet, states)
+        if state in tweets_by_state:
+            tweets_by_state[state].append(tweet)
+        else:
+            tweets_by_state[state] = [tweet]
+
+    for state in tweets_by_state:
+        for tweet in tweets_by_state[state]:
+            tokens = tweet.message().split(' ')
+            for token in tokens:
+                if token.startswith('#'):
+                    if token in tags_by_state_with_freq[state]:
+                        tags_by_state_with_freq[state][token] += 1
+                    else:
+                        tags_by_state_with_freq[state][token] = 1
+
+    for state in tags_by_state_with_freq:
+        max_count = 0
+        max_tag = ''
+        for tag in tags_by_state_with_freq[state]:
+            if tags_by_state_with_freq[state][tag] > max_count:
+                max_count = tags_by_state_with_freq[state][tag]
+                max_tag = tag
+        most_pop_tag_by_state[state] = [max_tag, max_count]
+
+    print most_pop_tag_by_state
+
+# load all tweets
 def main():
-    input_terms = raw_input("Please enter search your filter terms separated by a comma: ")
-    filter_terms = input_terms.split(',')
-    sentiments = load_sentiments()
+    # input_terms = raw_input("Please enter search your filter terms separated by a comma: ")
+    # filter_terms = input_terms.split(',')
+    # sentiments = load_sentiments()
     states = load_states()
-    # load all tweets
     tweets = load_tweets('data/tweets_with_time.json')
-    sentiment_color_map = map_sentiment_to_state(tweets, sentiments, states, filter_terms)
+    # sentiment_color_map = map_sentiment_to_state(tweets, sentiments, states, filter_terms)
+
+    most_popular_hashtag_by_state(tweets, states)
+
+    '''
     usa = Country(states, 1200)
 
     for state in sentiment_color_map:
         usa.setFillColor(state, sentiment_color_map[state])
+    '''
 
 main()
